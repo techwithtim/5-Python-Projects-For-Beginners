9ba5ce543a23e6d23ece976abbbf3b6b50a3463e
ajonas <Jonas.Albert@madiso.hu>
2022-09-13 14:28:40 +0200

    Harmadik átalakítás - Hibák javítása (keressétek meg őket!)

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,9 +1,8 @@
 import rps_match
+from rps_match import options
 
 user_wins = 0
 computer_wins = 0
-
-options = ["rock", "paper", "scissors"]
 
 while True:
     user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
@@ -13,7 +12,7 @@ while True:
     if user_input not in options:
         continue
 
-    if rps_match(user_input) == "user_wins":
+    if rps_match.rps_match(user_input) == "user_wins":
         print("You won!")
         user_wins += 1
     else:
