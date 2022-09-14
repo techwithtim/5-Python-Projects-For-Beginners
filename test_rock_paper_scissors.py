1971f642db9ef59bcf701c73123e1f9c8a245fce
Albert <jonas.albert.oktatas@gmail.com>
2022-09-14 19:44:42 +0200

    Hozzáadva 8 másik teszt

--- test_rock_paper_scissors.py
+++ test_rock_paper_scissors.py
@@ -3,9 +3,48 @@ import random
 
 from unittest.mock import MagicMock
 
-random.randint = MagicMock(return_value=0)
 
-
-def test_rps_match():
-    #    computer_pick = "rock"
+def test_rps_match_paper_rock():
+    random.randint = MagicMock(return_value=0)
     assert rps_match.rps_match("paper") == "user_wins"
+
+
+def test_rps_match_paper_paper():
+    random.randint = MagicMock(return_value=1)
+    assert rps_match.rps_match("paper") == "computer_wins"
+
+
+def test_rps_match_paper_scissors():
+    random.randint = MagicMock(return_value=2)
+    assert rps_match.rps_match("paper") == "computer_wins"
+
+
+def test_rps_match_scissors_rock():
+    random.randint = MagicMock(return_value=0)
+    assert rps_match.rps_match("scissors") == "computer_wins"
+
+
+def test_rps_match_scissors_paper():
+    random.randint = MagicMock(return_value=1)
+    assert rps_match.rps_match("scissors") == "user_wins"
+
+
+def test_rps_match_scissors_scissors():
+    random.randint = MagicMock(return_value=2)
+    assert rps_match.rps_match("scissors") == "computer_wins"
+
+
+def test_rps_match_rock_rock():
+    random.randint = MagicMock(return_value=0)
+    assert rps_match.rps_match("rock") == "computer_wins"
+
+
+def test_rps_match_rock_paper():
+    random.randint = MagicMock(return_value=1)
+    assert rps_match.rps_match("rock") == "computer_wins"
+
+
+def test_rps_match_rock_scissors():
+    random.randint = MagicMock(return_value=2)
+    assert rps_match.rps_match("rock") == "user_wins"
+
