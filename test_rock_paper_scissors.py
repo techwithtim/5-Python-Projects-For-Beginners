a2229edc7b294c038ac7d976a4ca23f455ebf8ec
Albert <jonas.albert.oktatas@gmail.com>
2022-09-14 20:15:24 +0200

    Hibajavítás: duplikált hívás javítva

--- test_rock_paper_scissors.py
+++ test_rock_paper_scissors.py
@@ -11,7 +11,7 @@ def test_rps_match_paper_rock():
 
 def test_rps_match_paper_paper():
     random.randint = MagicMock(return_value=1)
-    assert rps_match.rps_match("paper") == "computer_wins"
+    assert rps_match.rps_match("paper") == "nobody wins"
 
 
 def test_rps_match_paper_scissors():
@@ -31,12 +31,12 @@ def test_rps_match_scissors_paper():
 
 def test_rps_match_scissors_scissors():
     random.randint = MagicMock(return_value=2)
-    assert rps_match.rps_match("scissors") == "computer_wins"
+    assert rps_match.rps_match("scissors") == "nobody wins"
 
 
 def test_rps_match_rock_rock():
     random.randint = MagicMock(return_value=0)
-    assert rps_match.rps_match("rock") == "computer_wins"
+    assert rps_match.rps_match("rock") == "nobody wins"
 
 
 def test_rps_match_rock_paper():
