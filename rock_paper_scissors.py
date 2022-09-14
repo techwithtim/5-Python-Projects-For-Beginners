acb6cc395c31256751e874e0d3f5d4184be6f717
Albert <jonas.albert.oktatas@gmail.com>
2022-09-14 20:24:59 +0200

    Függvényként kiemelve a menü

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,30 +1,3 @@
-import rps_match
-from rps_match import options
+import game
 
-user_wins = 0
-computer_wins = 0
-
-while True:
-    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
-    if user_input == "q":
-        break
-
-    if user_input not in options:
-        continue
-
-    result = rps_match.rps_match(user_input)
-
-    if result == "user_wins":
-        print("You won!")
-        user_wins += 1
-
-    elif result == "computer_wins":
-        print("You lost!")
-        computer_wins += 1
-    else:
-        print("Nobody wins!")
-
-
-print("You won", user_wins, "times.")
-print("The computer won", computer_wins, "times.")
-print("Goodbye!")
+game.game()
