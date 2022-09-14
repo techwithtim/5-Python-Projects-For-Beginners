a9f4d75f7f7c8de6b0567bb34d66a2510fdb9a88
Albert <jonas.albert.oktatas@gmail.com>
2022-09-14 18:58:13 +0200

    Mocking hozzáadva

--- test_rock_paper_scissors.py
+++ test_rock_paper_scissors.py
@@ -1,7 +1,11 @@
 import rps_match
+import random
+
+from unittest.mock import MagicMock
+
+random.randint = MagicMock(return_value=0)
 
 
 def test_rps_match():
-    computer_pick = "rock"
-    assert rps_match.rps_match("paper") == "You won!"
-
+    #    computer_pick = "rock"
+    assert rps_match.rps_match("paper") == "user_wins"
