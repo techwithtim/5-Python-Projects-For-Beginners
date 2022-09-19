817b818d7d29b556a7f3b629b2fcf0cd2ea83425
Albert <jonas.albert.oktatas@gmail.com>
2022-09-19 20:04:44 +0200

    100-al oszthatóság bevezetve

--- leap_year.py
+++ leap_year.py
@@ -2,7 +2,9 @@
 #kimenet pedig igaz vagy hamis, igaz, ha szökőév, hamis, ha nem az.
 
 def leap_year(year: int):
-    if year % 4 == 0:
+    if year % 100 == 0:
+        return False
+    elif year % 4 == 0:
         return True
     else:
         return False
--- test_leap_year.py
+++ test_leap_year.py
@@ -8,6 +8,14 @@ def test_leap_year_divisible_by_4():
     assert leap_year(16) == True
 
 
+def test_leap_year_divisible_by_100():
+    assert leap_year(100) == False
+    assert leap_year(200) == False
+    assert leap_year(500) == False
+    assert leap_year(1000) == False
+    assert leap_year(2000) == False
+
+
 
 
 
