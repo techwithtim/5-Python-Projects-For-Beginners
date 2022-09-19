1b65e064938022a92f4c60233257130486a71625
Albert <jonas.albert.oktatas@gmail.com>
2022-09-19 19:37:04 +0200

    Függvény csontváz megadva

--- leap_year.py
+++ leap_year.py
@@ -1,3 +1,6 @@
 #függvény, aminek egy bemenete van, és egy kimenete, a bemenet az évszám,
 #kimenet pedig igaz vagy hamis, igaz, ha szökőév, hamis, ha nem az.
 
+def leap_year(year: int):
+    return
+
