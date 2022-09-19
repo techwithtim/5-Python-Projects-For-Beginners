bb0cbe3a16022e884c37431140fd6900fe4079e5
Albert <jonas.albert.oktatas@gmail.com>
2022-09-19 20:13:40 +0200

    400-al oszthatóság bevezetve

--- leap_year.py
+++ leap_year.py
@@ -2,7 +2,9 @@
 #kimenet pedig igaz vagy hamis, igaz, ha szökőév, hamis, ha nem az.
 
 def leap_year(year: int):
-    if year % 100 == 0:
+    if year % 400 == 0:
+        return True
+    elif year % 100 == 0:
         return False
     elif year % 4 == 0:
         return True
--- test_leap_year.py
+++ test_leap_year.py
@@ -13,9 +13,13 @@ def test_leap_year_divisible_by_100():
     assert leap_year(200) == False
     assert leap_year(500) == False
     assert leap_year(1000) == False
-    assert leap_year(2000) == False
 
 
-
+def test_leap_year_divisible_by_400():
+    assert leap_year(400) == True
+    assert leap_year(800) == True
+    assert leap_year(1200) == True
+    assert leap_year(1600) == True
+    assert leap_year(2000) == True
 
 
