b3b7006e0d260d22b338a7180529e070c44c348e
Albert <jonas.albert.oktatas@gmail.com>
2022-09-21 18:22:23 +0200

    Input-output mocking hozzáadva, dummy teszt hozzáadva

---
+++ tud_test_base.py
@@ -0,0 +1,32 @@
+import builtins
+
+input_values = []
+print_values = []
+
+
+def mock_input(s):
+    print_values.append(s)
+    return input_values.pop(0)
+
+
+def mock_input_output_start():
+    global input_values, print_values
+
+    input_values = []
+    print_values = []
+
+    builtins.input = mock_input
+    builtins.print = lambda s: print_values.append(s)
+
+
+def get_display_output():
+    global print_values
+    return print_values
+
+
+def set_keyboard_input(mocked_inputs):
+    global input_values
+
+    mock_input_output_start()
+    input_values = mocked_inputs
+
