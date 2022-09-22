7375993a1fe6a85f2d6e98f696550fc1a9f7ce2e
Agicka <agi.varga@outlook.com>
2022-09-22 17:59:09 +0200

    anagram függvény javítva

--- anagram_fuggveny.py
+++ anagram_fuggveny.py
@@ -1,10 +1,5 @@
-def anagram_fuggveny(str1, str2):
+def anagram_fuggveny():
     if(sorted(str1) == sorted(str2)):
         return "Two Strings are Anagrams."
     else:
         return "Two Strings are not Anagrams."
-
-str1 = input("Enter the First String  = ")
-str2 = input("Enter the Second String = ")
-result = anagram_fuggveny(str1, str2)
-print(anagram_fuggveny(str1, str2))
